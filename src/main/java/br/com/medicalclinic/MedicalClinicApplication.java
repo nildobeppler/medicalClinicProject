5312b3269b2b9a4d0e8412fb7fd4e08aa42b5815
Nildo Beppler <Nildo Beppler@10.6.1.149>
2017-05-31 19:27:38 -0300

    Config projeto

--- src/main/java/br/com/medicalclinic/MedicalClinicApplication.java
+++ src/main/java/br/com/medicalclinic/MedicalClinicApplication.java
@@ -2,11 +2,25 @@ package br.com.medicalclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class MedicalClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedicalClinicApplication.class, args);
+	}
+
+	@Configuration
+	@EnableWebMvc
+	public class WebConfig extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**");
+		}
 	}
 }
--- src/main/java/br/com/medicalclinic/resources/DoctorResources.java
+++ src/main/java/br/com/medicalclinic/resources/DoctorResources.java
@@ -11,6 +11,7 @@ import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,6 +56,7 @@ public class DoctorResources {
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@CrossOrigin
 	public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
 		doctorService.delete(id);
 		return ResponseEntity.noContent().build();
